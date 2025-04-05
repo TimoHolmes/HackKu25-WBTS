c1b37a2a66318dddeef8b8d66d717b630f8e81b1
Timothy Holmes <holmestimo11@gmail.com>
2025-04-05 01:17:41 -0500

    added sqlite databases. for users and past_paths

--- Backend/sqlite.py
+++ Backend/sqlite.py
@@ -4,6 +4,7 @@ from typing import List, Tuple, Any
 from contextlib import closing
 from datetime import datetime
 from pathlib import Path
+from datetime import timedelta
 
 #Store file path for UserId, firstname, lastname, session token, past-paths
 connection = sqlite3.connect('hackku.db')
@@ -14,8 +15,17 @@ command1 = """ Create table if not exists users (
     FirstName string, 
     LastName string,
     Email string Not Null,
-    SessionToken string
+    SessionToken string,
+) """
+
+command2 =""" Create table if not exists past_paths
+(
+    UserId string,
+    Path TEXT NOT NULL,
+    CreatedAt DATETIME DEFAULT CURRENT_TIMESTAMP,
+    foreign key (UserId) references users(UserId)
 ) """
 
 cursor.execute(command1)
+cursor.execute(command2)
 connection.commit()
