72eaf3a3d28b333f17209b14778d7ae323fa0d09
spenceraddis <chubsezzz@gmail.com>
2025-04-04 23:07:26 -0500

    separated content view into multiple files

--- Front End/GPXApp/ContentView.swift
+++ Front End/GPXApp/ContentView.swift
@@ -67,82 +67,6 @@ struct ContentView: View {
     }
 }
 
-struct MapView: View {
-    @State private var position: MapCameraPosition = .automatic
-    @StateObject var deviceLocationService = DeviceLocationService.shared
-    @State var tokens: Set<AnyCancellable> = []
-    
-    // Coordinates for Lawrence, Kansas
-    @State var coordinates: (lat: Double, lon: Double) = (38.9717, -95.2353)
-    
-    // Route
-    @State private var showRoute = false
-    @State private var routeDisplaying = false
-    @State private var route: MKRoute?
-    @State private var routeDestination: MKMapItem?
-    @State private var travelInterval: TimeInterval?
-    @State private var transportType: MKDirectionsTransportType = .walking
-    var body: some View {
-        VStack {
-            Map(position: $position) {}
-                .mapStyle(.hybrid(elevation: .flat))
-        }
-        .task {
-            do {
-                let osm = try await getAsyncOSMData(lon: coordinates.lon, lat: coordinates.lat, radius: 10000)
-            } catch {
-                print("OSM Data fetch failed", error)
-            }
-        }
-        .onAppear {
-            observeCoordinateUpdates()
-            observeDeviceLocationDenied()
-            deviceLocationService.requestLocationUpdates()
-            
-            // Move map to Lawrence, Kansas on appearance
-            position = .region(
-                MKCoordinateRegion(
-                    center: CLLocationCoordinate2D(latitude: coordinates.lat, longitude: coordinates.lon),
-                    span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01)))
-        }
-    }
-    
-    
-    func observeCoordinateUpdates() {
-        deviceLocationService.coordinatesPublisher
-            .receive(on: DispatchQueue.main)
-            .sink { _ in } receiveValue: { coordinates in
-                self.coordinates = (coordinates.latitude, coordinates.longitude)
-            }
-            .store(in: &tokens)
-    }
-    
-    func observeDeviceLocationDenied() {
-        deviceLocationService.deniedLocationAccess
-            .receive(on: DispatchQueue.main)
-            .sink {
-                print("Error on getting location")
-            }
-            .store(in: &tokens)
-    }
-    
-    
-    func fetchRoute() async {
-        // Need to somehow fetch route
-    }
-}
-
-struct ProgressPage: View {
-    var body: some View {
-        VStack {
-            Text("Progress Page")
-                .font(.largeTitle)
-                .padding()
-            Spacer()
-        }
-    }
-}
-
 struct FooterButton: View {
     var iconName: String
     var label: String
@@ -159,30 +83,6 @@ struct FooterButton: View {
                     .foregroundColor(.white)
             }
             .frame(maxWidth: .infinity)
-        }
-    }
-}
-
-// New NavigationButton above footer with an oval shape
-struct NavigationButton: View {
-    var action: () -> Void
-    
-    var body: some View {
-        Button(action: action) {
-            HStack {
-                Image(systemName: "location.north.fill") // Directional arrow icon
-                    .font(.title2)
-                    .foregroundColor(.white)
-                Text("Create Route")
-                    .foregroundColor(.white)
-                    .font(.body)
-                    .padding(.leading, 5)
-            }
-            .padding()
-            .frame(height: 60)
-            .background(Color.blue)
-            .clipShape(Capsule()) // Oval shape
-            .shadow(radius: 5)
         }
     }
 }
